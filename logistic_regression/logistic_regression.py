7ef38a69d72113b55aec1b699032ccbfdead6693
Anders Kjelsrud <akjelsrud@gmail.com>
2022-09-06 19:22:40 +0200

    regression done

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -7,13 +7,14 @@ import pandas as pd
 class LogisticRegression:
     
     def __init__(self):
-        # NOTE: Feel free add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
+        self.w = None
+        self.b = None
+        self.losses = None
+        self.C = 0.1
         
-    def fit(self, X, y):
+    def fit(self, X, y, lr=0.01, batch_size = 16, epochs=1500, verbose=False):
         """
-        Estimates parameters for the classifier
+        Estimates parameters for the classifier with mini-batch gradient descent
         
         Args:
             X (array<m,n>): a matrix of floats with
@@ -21,8 +22,42 @@ class LogisticRegression:
             y (array<m>): a vector of floats containing 
                 m binary 0.0/1.0 labels
         """
-        # TODO: Implement
-        raise NotImplemented()
+        #setup
+        if X.ndim == 1:
+            n = 1
+        else:
+            n = X.shape[1]
+        m = X.shape[0]
+        X = np.array(X)
+        y = np.array(y)
+        self.w = np.zeros(n)
+        self.b = 0
+        self.losses = []
+        #training loop
+        for epoch in range(epochs):
+            #mini-batch gradient descent
+            for i in range((m-1)//batch_size + 1):
+                #get batch
+                X_batch = X[i*batch_size:(i+1)*batch_size]
+                y_batch = y[i*batch_size:(i+1)*batch_size]
+                #forward pass
+                z = np.einsum('ij,j->i', X_batch, self.w) + self.b
+                #apply activation function
+                y_pred = sigmoid(z)
+                #get the gradients
+                dldw, dldb = self.gradients(X_batch, y_batch, y_pred)
+                #do gradient descent
+                self.w = self.w - lr * dldw
+                self.b = self.b - lr * dldb
+            #compute loss and append to list
+            z = np.einsum('ij,j->i', X, self.w) + self.b
+            y_pred = sigmoid(z)
+            loss = binary_cross_entropy(y, y_pred)
+            self.losses.append(loss)
+            if verbose:
+                print(f"Epoch {epoch+1} loss: {loss}")  
+
+        return self.w, self.b
     
     def predict(self, X):
         """
@@ -38,9 +73,18 @@ class LogisticRegression:
             A length m array of floats in the range [0, 1]
             with probability-like predictions
         """
-        # TODO: Implement
-        raise NotImplemented()
+        #forward pass
+        z = np.einsum('ij,j->i', X, self.w) + self.b
+        y_pred = sigmoid(z)
+        return y_pred
         
+    def gradients(self, X, y, y_pred, C=0.1):
+        m = X.shape[0]
+        #gradient of loss wrt w
+        dldw = np.einsum('ij,i->j', X, y_pred - y) / m
+        #gradient of loss wrt b
+        dldb = np.sum((y_pred - y)) / m
+        return dldw, dldb
 
         
 # --- Some utility functions 
@@ -97,4 +141,7 @@ def sigmoid(x):
     """
     return 1. / (1. + np.exp(-x))
 
-        
+
+
+def normalize(x):
+    return (x - x.min()) / (x.max() - x.min())
